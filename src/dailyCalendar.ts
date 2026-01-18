4ad1b3761d185fbe51d3edf6776ed5c5c74f11bd
SeongJong <eeldclf25@gmail.com>
2026-01-18 11:48:04 +0900

    feat: add daily calendar support and UI integration

---
+++ src/dailyCalendar.ts
@@ -0,0 +1,46 @@
+import * as vscode from 'vscode';
+
+export class DailyCalendarProvider implements vscode.TextDocumentContentProvider {
+    private changeEvent = new vscode.EventEmitter<vscode.Uri>();
+    get onDidChange(): vscode.Event<vscode.Uri> {
+        return this.changeEvent.event;
+    }
+
+    private virtualFileContent = "";
+    provideTextDocumentContent(uri: vscode.Uri): string {
+        return this.virtualFileContent;
+    }
+
+    public async openDailyCalendar() {
+        if (!vscode.extensions.getExtension("Markwhen.markwhen")) {
+            vscode.window.showErrorMessage('This feature requires the Markwhen extension.');
+            return;
+        }
+
+        if (!vscode.workspace.workspaceFolders) {
+            vscode.window.showWarningMessage('No workspace folder open.');
+            return;
+        }
+
+        try {
+            const dailyNotesDir = vscode.workspace.getConfiguration("extext").get<string>("dailyNotesDirectory");
+            const files = await vscode.workspace.findFiles(new vscode.RelativePattern(vscode.workspace.workspaceFolders[0], `${dailyNotesDir}/*.{md,mw}`));
+
+            const decoder = new TextDecoder();
+            const combinedContent = (await Promise.all(
+                files.map(async (fileUri) => {
+                    const fileContent = await vscode.workspace.fs.readFile(fileUri);
+                    return decoder.decode(fileContent);
+                })
+            )).join('\n');
+
+            const uri = vscode.Uri.parse('markwhen:Daily Calendar.mw');
+            this.virtualFileContent = combinedContent;
+            this.changeEvent.fire(uri);
+
+            await vscode.commands.executeCommand('vscode.openWith', uri, 'markwhen.timeline', vscode.ViewColumn.Two);
+        } catch (error) {
+            vscode.window.showWarningMessage(`Error reading calendar files: ${error}`);
+        }
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,15 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { DailyCalendarProvider } from './dailyCalendar';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const provider = new DailyCalendarProvider();
+	const markwhenProvider = vscode.workspace.registerTextDocumentContentProvider("markwhen", provider);
+	const dailyCalendar = vscode.commands.registerCommand("extext.openDailyCalendar", () => provider.openDailyCalendar());
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "extension-extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('extension-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Extension Extension!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		markwhenProvider,
+		dailyCalendar
+	);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
